886b6eb67dcad14753387ab8f23bc5804dc5a5fe
Mahmoud S. ElGammal <m.s.elgammal@outlook.com>
2025-06-21 14:47:14 +0300

    update: add coudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "http://melgammal.com",
+  output: "static",
+  adapter: cloudflare(),
   integrations: [
     mdx(),
     sitemap(),
